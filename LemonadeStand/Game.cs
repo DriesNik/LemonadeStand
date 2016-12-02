f2616307b3783192962c1876eb19c0727bab3b07
Nik Dries <ninja1704@hotmail.com>
2016-12-02 08:03:14 -0600

    Basic Skeleton

---
+++ LemonadeStand/Game.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Game
+    {
+        public void StartGame()
+        {
+            StartIntro();
+            GatherInfo();
+            SetFinites();
+            ShowFinalProfit();
+            DayByDayLoop();
+        }
+        private void DayByDayLoop()
+        {
+            GenerateSetting();
+            GetRecipe();
+            CheckInventory();
+            GenerateClients();
+            RunDay();
+            MoneyAdding();
+        }
+        private void SetFinites()
+        {
+            throw new NotImplementedException();
+        }
+        private void ShowFinalProfit()
+        {
+            throw new NotImplementedException();
+        }
+        private void MoneyAdding()
+        {
+            throw new NotImplementedException();
+        }
+        private void RunDay()
+        {
+            throw new NotImplementedException();
+        }
+        private void GenerateClients()
+        {
+            throw new NotImplementedException();
+        }
+        private void CheckInventory()
+        {
+            throw new NotImplementedException();
+        }
+        private void GetRecipe()
+        {
+            throw new NotImplementedException();
+        }
+        private void GenerateSetting()
+        {
+            throw new NotImplementedException();
+        }
+        private void GatherInfo()
+        {
+            throw new NotImplementedException();
+        }
+        private void StartIntro()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LemonadeStand/Player.cs
+++ LemonadeStand/Player.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace LemonadeStand
 {
-    class Program
+    class Player
     {
-        static void Main(string[] args)
-        {
-            Game LS = new Game();
-            LS.StartGame();
-        }
     }
 }
